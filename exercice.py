d42b3349f52de5445a960969a047fefbe5b59989
phhad <philippe.hadjis@polymtl.ca>
2021-03-09 13:45:13 -0500

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -5,29 +5,17 @@
 
 
 import math
+import sys
+sys.path(0, "")
 # TODO: Définissez vos fonction ici
 def ellipse(a,b,c,ri):
     volume_ell = math.pi()*a*b*c*(4/3)
     masse_ell = ri*volume_ell
     return volume_ell, masse_ell
 
-def frequence(sentence: str) -> dict:
-    # TODO: Afficher les lettres les plus fréquentes
-    #       Retourner le tableau de lettres
-
-    frequency = dict()
-    for letter in sentence:
-        frequency[letter] = sentence.count(letter)
-
-    sorted_keys = sorted(frequency, key=frequency.__getitem__, reverse=True)
-    for key in sorted_keys:
-        if frequency[key] > 5:
-            print(f"Le caractère {key} revient {frequency[key]} fois.")
-
-    return frequency
-
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
     mass_vol = ellipse()
     print(mass_vol)
+    lambda sentence: (frequence(sentence), sorted(frequency, key=frequency.__getitem__
     pass
